80cf1d145d2aa46b321a5cd378f83944c2c97d79
Francis TSAFACK <wilfrancis@yahoo.fr>
2025-09-30 23:52:12 +0200

    feat: init EFCore for user
    test
    
    readme.md
    
    readme.md
    
    readme.md
    
    feat: init EFCore for user
    
    format code
    
    feat: BIZ-12 format code

---
+++ microservices/user/Bizca.Users.Infrastructure/Context/Configurations/CivilityEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Bizca.Users.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bizca.Users.Infrastructure.Context.Configurations;
+
+public class CivilityEntityConfiguration : IEntityTypeConfiguration<Civility>
+{
+	public void Configure(EntityTypeBuilder<Civility> builder)
+	{
+		builder.HasKey(e => e.CivilityId).HasName("pk_civility");
+
+		builder.Property(e => e.CivilityId).ValueGeneratedNever();
+		builder.Property(e => e.CreationDate).HasDefaultValueSql("(getdate())");
+		builder.Property(e => e.LastUpdate).HasDefaultValueSql("(getdate())");
+	}
+}
